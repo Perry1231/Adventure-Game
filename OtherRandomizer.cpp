e83c2bc919041a110b6f4e224f49a17bf923fd2d
Vladyslav <bravilmaksim@gmail.com>
2026-06-26 11:39:51 +0300

    Commit 58 (Additional randomizer)

---
+++ OtherRandomizer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Header.h"
+//Randomizer for chests and another items
+
+/* void OtherRandomizer::RandomizeChest() {
+    // Generate random items for the chest
+}
+
+*/
