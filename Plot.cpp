3896e21d63ace9fb071a18ac70bc0154a660319e
Vladyslav <bravilmaksim@gmail.com>
2026-06-30 23:07:06 +0300

    Commit 84 (Just started Plot.cpp)

--- Plot.cpp
+++ Plot.cpp
@@ -1,1 +1,10 @@
 #include "Header.h"
+#include <iostream>
+//For main events and plot
+
+void StartGame()
+{
+    std::cout << "\n===Welcome to the Adventure===" << std::endl;
+    std::cout << "Your journey begins..." << std::endl;
+    
+}
