de0f2c12435c6ce28d72bff12efa4b4c36d77e68
Vladyslav <bravilmaksim@gmail.com>
2026-06-30 23:14:16 +0300

    Commit 85 (Added some extension)

--- OtherCharacter.cpp
+++ OtherCharacter.cpp
@@ -1,3 +1,30 @@
+#include <string>
 #include <iostream>
 #include "Header.h"
 //File for additional characters
+
+
+void QuestGiver()
+{
+
+}
+
+
+
+void Merchants()
+{
+
+}
+
+
+void Allies()
+{
+
+}
+
+
+void Antagonists()
+{
+}
+
+
