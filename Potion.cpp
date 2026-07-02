3b49ddd7d8d0a6c042d5605c493d843d04721363
Vladyslav <bravilmaksim@gmail.com>
2026-07-02 21:01:15 +0300

    Commit 95 (Added Potion.cpp)

---
+++ Potion.cpp
@@ -0,0 +1,25 @@
+#include "Header.h"
+#include <map>
+#include <string>
+
+struct PotionData {
+    std::string name;
+    int effectType; // 0=HP, 1=STR, 2=AGI...
+    int potency;
+    int duration;
+};
+
+
+static std::map<std::string, std::tuple<int, int, int>>& GetPotionDB() {                 //For spell list
+    static std::map<std::string, std::tuple<int, int, int>> pt;
+    if (pt.empty()) {
+        pt = {
+        {"Health Potion", {0, 20, 0}},
+        {"Strength Potion", {1, 5, 3}},
+        {"Poison Potion", {0, -10, 0}},
+        {"Weakness Potion", {1, -5, 3}},
+        {"Slowness Potion", {2, -1, 3}},
+        {"Agility Potion", {2, 5, 3}}};
+    }
+    return pt;
+}
