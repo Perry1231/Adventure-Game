ce34e8a8be3e1b2815f4a59434f5cd2479faeffb
Vladyslav <bravilmaksim@gmail.com>
2026-07-02 21:41:38 +0300

    Commit 100 (Added morefixes to MainFunctions.cpp and Inventory.cpp)

--- Inventory.cpp
+++ Inventory.cpp
@@ -125,19 +125,18 @@ else if (choice == 4) {
             if (items[i]->GetType() == "Potion") {
                 Potion* potion = dynamic_cast<Potion*>(items[i]);
                 potion->ApplyEffect(owner);
-                if (potion->IsEquipped()) {
-                    potion->Reset();
+                DelItem(itemId);
+            break;
                 }
             } else {
                 std::cout << "Cannot use directly!" << std::endl;
             }
         }
     }
-}//Else if choice not good
         else if (choice == 0) continue;
         else std::cout << "Invalid choice!" << std::endl;   
     }
-    }
+}
 }
 
 
