c0582fa653e07da14dc1767a1fcec862d71087f9
Vladyslav <bravilmaksim@gmail.com>
2026-07-26 15:56:48 +0300

    Commit 181 (Added first list of additional game-events)

--- Evets.cpp
+++ Evets.cpp
@@ -1,2 +1,121 @@
     #include "Header.h"
 //Additional events for game
+
+
+//Nature events========================================
+void ToxicRain()
+{
+
+}
+
+void Rain() //Heavy + light
+{
+
+}
+
+void VolcanicEruption()
+{
+
+}
+
+void FlashFlood()
+{
+
+}
+
+void Mudslide()
+{
+
+}
+
+void HumidHot()
+{
+
+}
+
+void ConstrictorVines()
+{
+
+}
+
+void WildMagicWilds()
+{
+
+}
+
+void QuickFreezingGlad()
+{
+
+}
+
+void WillWispAttraction()
+{
+
+}
+
+void DeadMagicZone()
+{
+
+}
+
+void PollenHallucinations ()
+{
+
+}
+
+
+//Man events===========================================
+void EnemyPatrol()
+{
+
+} 
+
+void MonsterAmbush()
+{
+
+}
+
+void CunningScavengers()
+{
+
+}
+
+void StagedAccident()
+{
+
+}
+
+void FalseHospitableHost()
+{
+
+}
+
+void ThePropheticBeggar()
+{
+
+}
+
+void TheShadowingKid()
+{
+
+}
+
+void Extortion()
+{
+
+}
+
+void TheTurncoatGuide()
+{
+
+}
+//NonLive events=================================
+void PoacherTrapTrigger()
+{
+
+}
+
+void FakeDistressCal()
+{
+
+}
