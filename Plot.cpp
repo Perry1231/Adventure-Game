029babf82924ea8f8ef286d3576cbb9f7bde9aed
Vladyslav <bravilmaksim@gmail.com>
2026-08-03 17:27:11 +0300

    Commit 204 (Plot.cpp  add)

--- Plot.cpp
+++ Plot.cpp
@@ -17,9 +17,38 @@ void StartGame()
 }
 
 
-void Turn()
+void Turn(Character& hero)
 {
-    //main mechanics
+    std::cout << "\n\n" ;
+    std::cout <<"\n=== TURN ===\n\n\n" << std::endl;
+    std::cout <<"----------------"<< std::endl;
+    std::cout <<"Make turn - 1" << std::endl;
+    std::cout <<"Look in inventory - 2" << std::endl;
+    std::cout <<"Go to main menu - 3" << std::endl;
+    std::cout <<"----------------"<< std::endl;
+
+    std::cout << "Enter choice : " ;
+    int choice = 0;
+    std::cin >> choice;
+    switch(choice)
+    {
+        case 1:
+            std::cout << "You have chosen to make a turn." << std::endl;
+            // Implement turn logic here
+            break;
+        case 2:
+            std::cout << "You have chosen to look in your inventory." << std::endl;
+            hero.GetInventory()->DisplayInventory(); {
+            break;
+        case 3:
+            std::cout << "Returning to main menu." << std::endl;
+            MainFunction(); 
+            break;
+        default:
+            std::cout << "Invalid choice. Please select a valid option." << std::endl;
+            break;
+    }
     //events % randomizer
     std::cout << "\n\n" ;
+}
 }
