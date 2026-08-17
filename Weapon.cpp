75c61710811901fa415fa9bedb641fb7e392666d
Vladyslav <bravilmaksim@gmail.com>
2026-08-17 16:47:29 +0300

    Commit 262 (Added Artifact file and list)

--- Weapon.cpp
+++ Weapon.cpp
@@ -308,46 +308,15 @@ void Weapon::Reset()
     return dp;
 }
 
- static std::map<std::string, std::pair<int, int>>& GetWeaponArtifactDB() {                 //For artifacts list
-    static std::map<std::string, std::pair<int, int>> ar;
-    if (ar.empty()) {
-        ar = {
-            {"Midas arm", {10, 10}},
-            {"Javelin of Backbiting", {10, 10}},
-            {"Wand of Orcus", {10, 10}},
-            {"Windvane", {10, 10}},
-            {"Spear of Heliod", {10, 10}},
-            {"Ephixis", {10, 10}},
-            {"Two-Birds Sling", {10, 10}},
-            {"Axe of the Dwarvish Lords", {10, 10}},
-            {"Gambler’s Blade", {10, 10}},
-            {"Orcsplitter ", {10, 10}},
-            {"Sword of Sharpness", {10, 10}},
-            {"Vorpal Sword", {10, 10}},
-            {"Acheron Blade", {10, 10}},
-            {"Luck Blade", {10, 10}},
-            {"Sword of Life Stealing", {10, 10}},
-            {"Mind Lash", {10, 10}},
-            {"Dragon Slayer", {10, 10}}};
-    }
-    return ar;
-}
-
 //For chest =====================================================================================================================================================================
 std::string  Weapon::RandomizeWeaponSpeacial()                                                            
 {
     auto& dp = GetWeaponSpeacialDB();
-    auto& ar = GetWeaponArtifactDB();
+  
 
 std::string weapon_sp[] = {"Blade of Broken Mirrors","Javelin of Backbiting", "Wand of Orcus","Windvane", "Spear of Heliod","Ephixis", 
     "Two-Birds Sling","Axe of the Dwarvish Lords","Gambler's Blade","Orcsplitter ", "Sword of Sharpness","Vorpal Sword", "Acheron Blade",  
     "Luck Blade","Sword of Life Stealing","Mind Lash", "Dragon Slayer"};                                                                     //Special_weapon
-std::string weapon_ar[] = {"Midas arm", "Javelin of Backbiting", "Wand of Orcus", "Windvane",  "Spear of Heliod",  "Ephixis",  "Two-Birds Sling",  "Axe of the Dwarvish Lords", 
-                           "Gambler's Blade", "Orcsplitter ","Sword of Sharpness", "Vorpal Sword", "Acheron Blade","Luck Blade", "Sword of Life Stealing", "Mind Lash", "Dragon Slayer"}; //For_artifacts
-
-int random_number = rand() % 2; // Randomly choose between special weapon and artifact
-if(random_number == 0)
-{
     std::string chosen_name; 
         chosen_name = weapon_sp[rand() % 17];
         auto it = dp.find(chosen_name);
@@ -358,18 +327,4 @@ if(random_number == 0)
     
     name_w = chosen_name;
     return name_w;
-}
-else 
-{
- std::string chosen_name; 
-        chosen_name = weapon_ar[rand() % 17];
-        auto it = ar.find(chosen_name);
-        if (it != ar.end()) {
-            attack_w = it->second.first;
-            value_w = it->second.second;
-        }  
-    name_w = chosen_name;
-    return name_w;
-}
-
 }
